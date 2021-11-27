3938316c9c13fa4c76cc0406a3b1b3492aa3d319
Charptr0 <charlesofficial1@hotmail.com>
2021-11-26 22:55:58 -0500

    remove fragments

--- src/main/java/com/charptr0/simplereminders/MainActivity.java
+++ src/main/java/com/charptr0/simplereminders/MainActivity.java
@@ -1,24 +1,10 @@
 package com.charptr0.simplereminders;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.StrictMode;
 import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.charptr0.simplereminders.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,27 +20,5 @@ public class MainActivity extends AppCompatActivity {
     public void creationButtonHandler(View view){
         Intent intent = new Intent(MainActivity.this, CreationActivity.class);
         startActivity(intent);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
