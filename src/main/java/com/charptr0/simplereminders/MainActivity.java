9be84db357c6906e786644abf9ad67c5812f9fa4
Charptr0 <charlesofficial1@hotmail.com>
2021-12-06 20:02:56 -0500

    added static notification

--- src/main/java/com/charptr0/simplereminders/MainActivity.java
+++ src/main/java/com/charptr0/simplereminders/MainActivity.java
@@ -1,17 +1,22 @@
 package com.charptr0.simplereminders;
 
 import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.StrictMode;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,17 +25,27 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String CHANNEL_ID = "Main Channel";
+    private int notification_id = 1;
     private ArrayList<Reminder>listOfReminders = new ArrayList<>();
+
     private TextView noReminderText;
     private RecyclerView recyclerView;
     private ReminderAdapter adapter;
-
     private DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //init notifications for Android Oreo or higher
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Main Notification", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
 
         noReminderText = findViewById(R.id.noReminderText);
 
@@ -115,7 +130,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        
+
         if(requestCode == 1 && resultCode == RESULT_OK)
         {
             //make sure data is not null
@@ -128,8 +143,21 @@ public class MainActivity extends AppCompatActivity {
             adapter.notifyDataSetChanged();
 
             databaseHelper.addReminder(r);
+            createNotification(r.getName());
         }
 
         if(!listOfReminders.isEmpty()) noReminderText.setVisibility(View.INVISIBLE);
+    }
+
+
+    private void createNotification(String reminderName)
+    {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID);
+        builder.setContentTitle("Simple Reminders");
+        builder.setContentText(reminderName);
+        builder.setSmallIcon(R.drawable.ic_launcher_foreground);
+
+        NotificationManagerCompat manager = NotificationManagerCompat.from(MainActivity.this);
+        manager.notify(1, builder.build());
     }
 }
